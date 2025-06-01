302aeba3aff99a8d0f0ca7e79e758a31dfc94e76
PIYUSH MISHRA <piyushhmishraaa@gmail.com>
2025-06-01 14:29:40 +0530

    authentication completed

---
+++ src/app/auth/sign-in/page.tsx
@@ -0,0 +1,10 @@
+
+import { SignInView } from "@/module/auth/ui/views/sign-in-view";
+
+const Page =()=>{
+    return(
+        <SignInView/>
+    )
+}
+
+export default Page;
---
+++ src/app/auth/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import { SignUpView } from "@/module/auth/ui/views/sign-up-view";
+
+const Page =()=>{
+    return(
+        <SignUpView/>
+    )
+}
+
+export default Page;
