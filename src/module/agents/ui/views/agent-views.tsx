aef6681ad76f1d404b33362cfd357e8b8dd44aa4
PIYUSH MISHRA <piyushhmishraaa@gmail.com>
2025-06-09 20:22:28 +0530

    modified meetings page

--- src/module/agents/ui/views/agent-views.tsx
+++ src/module/agents/ui/views/agent-views.tsx
@@ -1,59 +1,63 @@
-"use client"
-import { useSuspenseQuery } from "@tanstack/react-query"
-import { useTRPC } from "@/trpc/client"
-import { useTransition } from "react"
-import { LoadingState } from "@/components/loading-state"
-import { ErrorState } from "@/components/error-state"
-import { DataTable } from "../components/data-table"
-import { columns  } from "../components/column"
-import { EmptyState } from "@/components/empty-state"
-import { useAgentsFilters } from "../../hooks/use-agents-filters"
-import { DataPagination } from "../components/data-pagination"
-import { useRouter } from "next/navigation"
+"use client";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { useTRPC } from "@/trpc/client";
+import { LoadingState } from "@/components/loading-state";
+import { ErrorState } from "@/components/error-state";
+import { columns } from "../components/column";
+import { EmptyState } from "@/components/empty-state";
+import { useAgentsFilters } from "../../hooks/use-agents-filters";
+import { DataPagination } from "../components/data-pagination";
+import { useRouter } from "next/navigation";
+import { DataTable } from "@/components/data-table";
 
- 
-export const AgentsView=()=>{
-    const router = useRouter();
+export const AgentsView = () => {
+  const router = useRouter();
 
-    const [filters,setFilters] = useAgentsFilters();
-    const trpc = useTRPC();
-    const {data} = useSuspenseQuery(trpc.agents.getMany.queryOptions({
-        ...filters
-    }));
-  
-    return (
-        <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4 ">
-           <DataTable
-            data={data.items} 
-            columns={columns}
-            onRowClick={(row)=>router.push(`/agents/${row.id}`)}
-           />
-           <DataPagination
-           page={filters.page}
-           totalPages = {data.totalPages}
-           onPageChange = {(page)=>setFilters({page})} />
-       {data.items.length===0 && (<EmptyState
-       title="Create your first agent"
-       description="Create Agents to join your meetings.Each agent will follow your instructions and will interact durig the calls"/>)}
-            {/* {JSON.stringify(data,null,2)} */}
-        </div>
-    ) 
-}
+  const [filters, setFilters] = useAgentsFilters();
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(
+    trpc.agents.getMany.queryOptions({
+      ...filters,
+    })
+  );
 
-export const AgentsViewLoading=()=>{
-    return(
-        <LoadingState
-        title="Loading Agents"
-        description="This may take a few seconds" />
+  return (
+    <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4 ">
+      <DataTable
+        data={data.items}
+        columns={columns}
+        onRowClick={(row) => router.push(`/agents/${row.id}`)}
+      />
+      <DataPagination
+        page={filters.page}
+        totalPages={data.totalPages}
+        onPageChange={(page) => setFilters({ page })}
+      />
+      {data.items.length === 0 && (
+        <EmptyState
+          title="Create your first agent"
+          description="Create Agents to join your meetings.Each agent will follow your instructions and will interact durig the calls"
+        />
+      )}
+      {/* {JSON.stringify(data,null,2)} */}
+    </div>
+  );
+};
 
-    )
-}
+export const AgentsViewLoading = () => {
+  return (
+    <LoadingState
+      title="Loading Agents"
+      description="This may take a few seconds"
+    />
+  );
+};
 
-export const AgentsViewError=()=>{
-    return(
-        <ErrorState
+export const AgentsViewError = () => {
+  return (
+    <ErrorState
       title="Error Loading Agents"
       description="Something went wrong"
     />
-    )
-}
+  );
+};
